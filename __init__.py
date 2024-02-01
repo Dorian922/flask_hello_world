b4e322eaee212e357552a43809f852a08c37bbfa
Dorian922 <151016520+Dorian922@users.noreply.github.com>
2024-02-01 14:05:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
+from flask import json
+from urllib.request import urlopen
 import sqlite3
 
 app = Flask(__name__)                                                                                                                  
@@ -8,6 +10,17 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 @app.route('/fr/')
 def hello_world_fr():
     return "<h2>Bonjour tout le monde !</h2>"
